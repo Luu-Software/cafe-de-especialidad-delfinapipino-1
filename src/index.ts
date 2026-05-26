c5333f4c30ef0a5ef9bff614243b51b59f3886aa
delfinapipino <delfinasolp@gmail.com>
2026-05-26 12:12:06 -0300

    empezado

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { preguntar } from './lib/consola.ts';
+ import { preguntar } from './lib/consola.ts';
 
 let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
 let totalPorPersona: number = 0; // COMPLETAR
-console.log(`Total por persona: $${totalPorPersona}`);
+let totalTexto: number = Number(totalTexto)
+let esEstudiante: string
+if esEstudiante = 'si'
+console.log(`Total por persona: $${totalPorPersona}`);s
